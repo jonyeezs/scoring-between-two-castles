aa3ea894b09e5dfa4c4dd0e1caaf6cf260126ad7
Jonathan Yee <jonyeezs@gmail.com>
2019-04-22 15:32:58 +1000

    Redo path to reflect a single castle

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'castles', pathMatch: 'full' },
-  { path: 'castles', loadChildren: './castle/castle.module#CastleModule' },
+  { path: '', redirectTo: 'castle', pathMatch: 'full' },
+  { path: 'castle', loadChildren: './castle/castle.module#CastleModule' },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
-import { FabbyComponent } from "./components/fabby/fabby.component";
+import { FabbyComponent } from './components/fabby/fabby.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
