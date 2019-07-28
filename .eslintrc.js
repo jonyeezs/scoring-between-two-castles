faffedfe0ebd53cbaf468963b37a80e1109f9eb1
Jonathan Yee <jonyeezs@gmail.com>
2019-07-28 23:10:18 +1000

    Fix issues with typescript-eslint v6

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,12 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-  },
+  env: { browser: true, es6: true },
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
   ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
+  globals: { Atomics: 'readonly', SharedArrayBuffer: 'readonly' },
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   parserOptions: {
@@ -19,8 +15,23 @@ module.exports = {
   },
   rules: {
     indent: ['error', 4],
+    indent: ['error', 2],
     'linebreak-style': ['error', 'unix'],
     quotes: ['error', 'single'],
     semi: ['error', 'always'],
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-member-accessibility': [
+      'warn',
+      {
+        accessibility: 'explicit',
+        overrides: {
+          accessors: 'explicit',
+          constructors: 'no-public',
+          methods: 'explicit',
+          properties: 'off',
+          parameterProperties: 'explicit',
+        },
+      },
+    ],
   },
 };
