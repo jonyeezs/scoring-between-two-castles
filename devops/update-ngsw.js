872d922474322a100ecef2c568f408337b21ae80
Jonathan Yee <jonyeezs@gmail.com>
2019-09-01 00:30:54 +1000

    ci(pipeline): update the version of the service worker appdata
    This will allow the CI to update the version of the app"

---
+++ devops/update-ngsw.js
@@ -0,0 +1,19 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const readPkg = require('read-pkg');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const updateJsonFile = require('update-json-file');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path');
+const filePath = path.resolve(`${process.cwd()}`, './src/ngsw-config.json');
+
+readPkg()
+  .then(package =>
+    updateJsonFile(filePath, ngsw => {
+      console.log(
+        `updating app version from ${ngsw.appData.version} to ${package.version}`
+      );
+      ngsw.appData.version = package.version;
+      return ngsw;
+    })
+  )
+  .then(() => console.log('ngsw updated'));
